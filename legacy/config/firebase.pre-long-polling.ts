38196f63c92639a88246b98b1a936da87595bf06
Isaiah <saibest730@gmail.com>
2026-07-12 21:41:59 -0400

    Improve Firestore connectivity on hosted app

--- legacy/config/firebase.pre-long-polling.ts
+++ legacy/config/firebase.pre-long-polling.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { initializeFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDwer-P7co_UNK-MkwyI24Ih8da7I1eB38",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "sidequest-2e798.firebaseapp.com",
@@ -11,11 +10,8 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1052439866179",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1052439866179:web:f0af3e29b8ca572f67ddf6"
 };
-
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const db = initializeFirestore(app, {
-  experimentalAutoDetectLongPolling: true
-});
+export const db = getFirestore(app);
 export const storage = getStorage(app);
+
