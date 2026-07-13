237d2ff93cf491536eb08b810788da19124adefd
Isaiah <saibest730@gmail.com>
2026-07-12 22:48:52 -0400

    Lock mobile layout to dynamic viewport

---
+++ legacy/config/main.pre-mobile-shell.tsx
@@ -0,0 +1,4 @@
+import{StrictMode}from"react";
+import{createRoot}from"react-dom/client";
+import AppV2 from"./v2/AppV2";
+createRoot(document.getElementById("root")!).render(<StrictMode><AppV2/></StrictMode>);
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
-import{StrictMode}from"react";
-import{createRoot}from"react-dom/client";
-import AppV2 from"./v2/AppV2";
-createRoot(document.getElementById("root")!).render(<StrictMode><AppV2/></StrictMode>);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppV2 from "./v2/AppV2";
+import "./v2/v2.css";
+import "./v2/mobile-shell.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppV2 />
+  </StrictMode>,
+);
