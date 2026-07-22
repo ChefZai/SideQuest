dd6c2b60df143c318da67f35bffaace8fec36c10
Isaiah <saibest730@gmail.com>
2026-07-21 21:47:18 -0400

    feat: complete SideQuest product polish pass

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./v2/warmth.css";
 import "./v2/responsive-shell.css";
 import "./v2/sunlit.css";
 import "./v2/onboarding.css";
+import "./v2/polish-system.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
